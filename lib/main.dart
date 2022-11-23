828e0c63fff6de38a275c71c790c26d46a9be57e
Saraswoti <sarasotprajapati@gmail.com>
2022-11-23 13:29:36 +0545

    adding images,googlefont,eleventedbutton loginpage

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
-      initialRoute: "/home",
+      //initialRoute: "/home",
       routes: {
         "/": (context) => LoginPage(),
         "/home": (context) => HomePage(),
