1fcdd88bf750646e65d06ef682f4926ee558cb97
Saraswoti <sarasotprajapati@gmail.com>
2022-11-23 12:34:16 +0545

    classes,route,finction

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   final int days = 30;
-  final String name = "Codepur";
+  final String name = "Learning Flutter";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Catalog App"),
+        title: Text("learning App"),
       ),
       body: Center(
         child: Container(
